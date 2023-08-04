f9ac8a7f49c880478a0beb00c484a1bc679be36f
Koushal <bhatkoushal2001@gmail.com>
2023-08-04 17:53:05 +0530

    Models

---
+++ java-api/src/main/java/com/db/grad/javaapi/controller/Book.java
@@ -0,0 +1,34 @@
+package com.db.grad.javaapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "book_name")
+    private String bookName;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+}
+
