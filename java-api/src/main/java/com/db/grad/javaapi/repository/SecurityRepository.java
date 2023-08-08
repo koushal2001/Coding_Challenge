a7bf572eef075e75c804c17d8d02bc4d39cd0f83
Koushal <bhatkoushal2001@gmail.com>
2023-08-09 01:36:45 +0530

    login controller added

--- java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.db.grad.javaapi.model.Security;
 import com.db.grad.javaapi.model.Trade;
@@ -11,5 +12,6 @@ import com.db.grad.javaapi.model.Trade;
 @Repository
 public interface SecurityRepository extends JpaRepository<Security, Long> {
         List<Security> findByMaturityDateBetween(LocalDate startDate, LocalDate endDate);
+        @Query("SELECT trade FROM Trade trade WHERE trade.security = :security")
         List<Trade> getTradesForSecurity(Long securityId);
 }
